2f36310487d43ffc14bcae84eab344ce49c806a3
Sebastian Ammon <seb.ammon@gmail.com>
2021-09-02 09:54:52 +0200

    Add error handling and improve README

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
 # Jesse's Graphics Card Scraper
 
 ## Getting started
+
 To get started follow the instructions below
 
-1. Install python
+1. Install python 3.x
 2. (Optional) Create a virtual env and activate it
 3. Run `pip install -r requirements.txt`
 
 ## How to use
-1. Navigate to [this page](https://www.wootware.co.za/computer-hardware/video-cards-video-devices/shopby/in_stock_with_wootware-stock_coming_soon?dir=asc&limit=100&order=price)
-2. CTRL + S (save the page) to this (root) directory as `page.html`
+
+1. Navigate
+   to [this page](https://www.wootware.co.za/computer-hardware/video-cards-video-devices/shopby/in_stock_with_wootware-stock_coming_soon?dir=asc&limit=100&order=price)
+2. CTRL + S (save the page) to this (root) directory as `page.html` (see screenshot below)
 3. (Optional) if you have a virtual env, activate it
 4. Delete the previous `products.csv` file
 5. Run `python main.py`
 6. Enjoy the `products.csv` file
+
+### Saving the webpage
+
+How to save the webpage dialog:
+
+![Save Webpage](assets/save-webpage.png)
--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 import codecs
 import csv
+import sys
+
 from bs4 import BeautifulSoup
 
 PRODUCTS_TO_FIND = ['RTX 3060']
 
 found_products = []
 
-with codecs.open('./page.html', 'r', 'utf-8') as page:
-    html_doc = page.read()
-    soup = BeautifulSoup(html_doc, 'html.parser')
+try:
+    with codecs.open('./page.html', 'r', 'utf-8') as page:
+        print('>> SCRAPING WEBPAGE')
 
-    products = soup.find_all('div', {'class': 'main-info'})
+        html_doc = page.read()
+        soup = BeautifulSoup(html_doc, 'html.parser')
 
-    for product in products:
-        title = product.find('h2').find('a').get_text()
-        price = product.find('p', {'class': 'special-price'}).find('span', {'class': 'price'}).get_text()
+        products = soup.find_all('div', {'class': 'main-info'})
 
-        for product_to_find in PRODUCTS_TO_FIND:
-            if product_to_find in title:
-                found_products.append({'title': title, 'price': price})
-                break
+        for product in products:
+            title = product.find('h2').find('a').get_text().strip()
+            price = product.find('p', {'class': 'special-price'}).find('span', {'class': 'price'}).get_text().strip()
 
-print('WRITING TO CSV')
+            for product_to_find in PRODUCTS_TO_FIND:
+                if product_to_find in title:
+                    found_products.append({'title': title, 'price': price})
+                    break
+except FileNotFoundError:
+    print(
+        'File `page.html` not found. Please save the webpage as an HTML file to this folder.' +
+        'Read the README for more information.')
+    sys.exit(1)
+
+print('>> WRITING TO CSV')
 
 with open('./products.csv', 'a') as file:
     writer = csv.writer(file)
@@ -29,4 +39,4 @@ with open('./products.csv', 'a') as file:
     for product in found_products:
         writer.writerow([product['title'], product['price']])
 
-print('DONE')
+print('>> DONE')
